ecc301c401010a580016af7567d14a13a3772947
Benard Joseph Tony <tonybenard16@gmail.com>
2025-09-22 08:38:02 +0100

    Modify changes to scrap_site.py and .gitignore

--- scrap_site.py
+++ scrap_site.py
@@ -43,33 +43,4 @@ class WikipediaScraper:
         
     
     def close(self):
-        self.driver.quit()
-
-
-
-
-
-
-
-
-
-
-# headers = {
-#  "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
-# }
-
-# class Website:
-#     def __init__(self, url):
-#         self.url = url
-#         response = requests.get(url, headers=headers)
-#         soup = BeautifulSoup(response.content, 'html.parser')
-#         self.title = soup.title.string if soup.title else "No title found"
-#         for irrelevant in soup.body(["script", "style", "img", "input"]):
-#             irrelevant.decompose()
-#         self.text = soup.body.get_text(separator="\n", strip=True)
-#         print(self.title)
-#         print(self.text)
-
-# content = Website("https://www.bbc.com/")
-    
-        
+        self.driver.quit()        
